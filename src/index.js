e7deb2a0eaf2b30beb6f0e2b1a357862dc988c2e
DavidTM <deivi_gf144@hotmail.com>
2021-07-10 16:22:16 -0400

    implementando cadastrar, listar e atualizar put e patch

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const { v4: uuidv4 } = require("uuid");
+const { json } = require("express");
 
 const app = express();
 
@@ -17,7 +18,7 @@ function checksExistsUserAccount(request, response, next) {
   const userFound = users.find((user) => user.username === username);
 
   if (!userFound) {
-    return response.status(200).json({ error: "user not found" });
+    return response.status(404).json({ error: "user not found" });
   }
 
   request.user = userFound;
@@ -40,18 +41,63 @@ app.post("/users", (request, response) => {
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user.todos) {
+    return response.status(204).json({ error: "no TODO for user selected" });
+  }
+
+  return response.status(200).json(user.todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { title, deadline } = request.body;
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  user.todos.push({
+    id: uuidv4(),
+    title: title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  });
+
+  return response.status(200).json(user);
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+  const { user } = request;
+
+  user.todos.map((todo) => {
+    if (todo.id === id) {
+      todo.title = title;
+      todo.deadline = new Date(deadline);
+    }
+  });
+
+  return response.status(200).json(user.todos);
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { id } = request.params;
+  const { user } = request;
+
+  user.todos.map((todo) => {
+    if (todo.id === id) {
+      todo.done = true;
+    }
+  });
+
+  return response.status(200).json(user);
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
